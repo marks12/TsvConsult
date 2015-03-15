bd2535187fa2bd4032a9e84b15919a8dd68dbe0c
marks12 <webts@bk.ru>
2015-03-15 23:51:12 +0700

    Save function

--- src/TsvConsult/Controller/TsvConsultAdminController.php
+++ src/TsvConsult/Controller/TsvConsultAdminController.php
@@ -10,6 +10,8 @@
 namespace TsvConsult\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use TsvConsult\Entity\TsvConsultation;
+use Zend\View\Model\JsonModel;
 
 class TsvConsultAdminController extends AbstractActionController
 {
@@ -23,5 +25,40 @@ class TsvConsultAdminController extends AbstractActionController
         // This shows the :controller and :action parameters in default route
         // are working when you browse to /tsvConsult/tsv-consult/foo
         return array();
+    }
+    
+    public function saveConsAction()
+    {
+    	$em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+    	
+    	$request_data = json_decode(file_get_contents("php://input"), true);
+
+    	$start_date = $request_data['start_date'];
+    	$end_date = $request_data['end_date'];
+        
+    	if (checkdate(explode("-", $start_date)[1], explode("-", $start_date)[0], explode("-", $start_date)[2]) 
+    	&& checkdate(explode("-", $end_date)[1], explode("-", $end_date)[0], explode("-", $end_date)[2])) 
+    	{
+
+    		$consultation = new TsvConsultation();
+    		$consultation->__set("start_date", explode("-", $start_date)[2]."-".explode("-", $start_date)[1]."-".explode("-", $start_date)[0]);
+    		$consultation->__set("end_date", explode("-", $end_date)[2]."-".explode("-", $end_date)[1]."-".explode("-", $end_date)[0]);
+    		$em->persist($consultation);
+    		$em->flush();
+    		
+    		$error = "ok";
+    	}
+    	else 
+    	{
+    		$error = "Одна из указанных дат не существует";
+    	}
+    	
+    	$result = new JsonModel(array(
+    			'success'	=>	true,
+    			'error'		=>	$error
+    	));
+
+    	
+    	return $result;
     }
 }
---
+++ src/TsvConsult/Entity/Content.php
@@ -0,0 +1,48 @@
+<?php
+namespace TsvConsult\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity */
+class TsvConsultation {
+	/**
+	 * @ORM\Id @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	
+	/** @ORM\Column(type="datetime") */
+	protected $date_start;
+	
+	/** @ORM\Column(type="datetime") */
+	protected $date_end;
+	
+    /**
+     * Magic getter
+     * @param $property
+     * @return mixed
+     */
+    public function __get($key)
+    {
+    	if(property_exists($this, $key))
+    	return $this->{$key};
+    	else
+    	die("Requested property {$key} not exists in ".__FUNCTION__." ".__CLASS__);
+    }
+
+    /**
+     * Magic setter
+     * @param $key
+     * @param $value
+     */
+    public function __set($key, $value)
+    {
+    	if(property_exists($this, $key))
+    	$this->{$key} = $value;
+    	else
+    	die("Requested property {$key} not exists in ".__FUNCTION__." ".__CLASS__);
+    }
+
+    public function __construct() {
+
+    }
+}
